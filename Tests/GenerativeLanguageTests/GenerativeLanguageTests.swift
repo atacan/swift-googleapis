0c422e5533a584a03b8fb255f0472d9b4c0d86cb
atacan <atacandur@icloud.com>
2025-02-27 23:06:19 +0100

    trying to test

--- Tests/GenerativeLanguageTests/GenerativeLanguageTests.swift
+++ Tests/GenerativeLanguageTests/GenerativeLanguageTests.swift
@@ -1,6 +1,29 @@
 import Testing
 @testable import GenerativeLanguage
 
+import GRPCCore
+import GRPCProtobuf
+import GRPCNIOTransportHTTP2
+
 @Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    try await withGRPCClient(
+        transport: .http2NIOPosix(
+            target: .ipv4(host: "generativelanguage.googleapis.com"),
+            transportSecurity: .tls
+        )
+    ) { client in
+        let greeter = Google_Ai_Generativelanguage_V1beta_GenerativeService.Client(wrapping: client)
+        let reply = try await greeter.generateAnswer(request: .init(message: .with({
+            $0.model = "gemini-2.0-flash"
+            $0.contents = [.with({
+                $0.role = "user"
+                $0.parts = [.with({
+                    $0.text = "Hello, world!"
+                })]
+            })]
+        })))
+        
+        dump(reply)
+    }
+    
 }
