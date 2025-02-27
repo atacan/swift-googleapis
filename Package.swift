175dc8f998bbcccf7536044101419558ebe24d35
atacan <atacandur@icloud.com>
2025-02-27 21:01:33 +0100

    Add initial Swift files for GenerativeLanguage package

--- Package.swift
+++ Package.swift
@@ -8,17 +8,17 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swift-googleapis",
-            targets: ["swift-googleapis"]),
+            name: "GenerativeLanguage",
+            targets: ["GenerativeLanguage"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-googleapis"),
+            name: "GenerativeLanguage"),
         .testTarget(
-            name: "swift-googleapisTests",
-            dependencies: ["swift-googleapis"]
+            name: "GenerativeLanguageTests",
+            dependencies: ["GenerativeLanguage"]
         ),
     ]
 )
