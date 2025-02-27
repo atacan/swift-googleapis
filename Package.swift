53514cd46f8c6cc1851cf042121a323d56deba5f
atacan <atacandur@icloud.com>
2025-02-27 21:29:18 +0100

    Add Package dependencies for gRPC and Protobuf support

--- Package.swift
+++ Package.swift
@@ -9,7 +9,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GenerativeLanguage",
-            targets: ["GenerativeLanguage"]),
+            targets: ["GenerativeLanguage"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.29.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "2.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
