fc3388d801879c961221f6b485ea7f019c01f2ab
atacan <atacandur@icloud.com>
2025-05-31 12:40:15 +0200

    Update .gitignore to include .env files and enhance GenerativeLanguageTests with a sample request for the generateContent method.

--- Tests/GenerativeLanguageTests/GenerativeLanguageTests.swift
+++ Tests/GenerativeLanguageTests/GenerativeLanguageTests.swift
@@ -1,5 +1,21 @@
 import Testing
-
+import Foundation
 @testable import GenerativeLanguage
 
-@Test func example() async throws {}
+@Test func example() async throws {
+    let request = Google_Ai_Generativelanguage_V1_GenerateContentRequest.with {
+        $0.model = "gemini-1.5-flash"
+        $0.contents = [
+            .with {
+                $0.parts = [
+                    .with {
+                        $0.text = "Hello, how are you?"
+                    }
+                ]
+                $0.role = GenerativeLanguageRole.user.rawValue
+            }
+        ]
+    }
+
+    
+}
