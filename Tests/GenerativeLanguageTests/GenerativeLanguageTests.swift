04bb6e349d598f2982a059ea08d44e91ba6d26a6
atacan <atacandur@icloud.com>
2025-05-31 12:42:14 +0200

    v1Beta

--- Tests/GenerativeLanguageTests/GenerativeLanguageTests.swift
+++ Tests/GenerativeLanguageTests/GenerativeLanguageTests.swift
@@ -3,7 +3,7 @@ import Foundation
 @testable import GenerativeLanguage
 
 @Test func example() async throws {
-    let request = Google_Ai_Generativelanguage_V1_GenerateContentRequest.with {
+    let request = Google_Ai_Generativelanguage_V1beta_GenerateContentRequest.with {
         $0.model = "gemini-1.5-flash"
         $0.contents = [
             .with {
